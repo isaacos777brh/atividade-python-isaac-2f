ec260ab1cf02cf224d26095d61b806e13179bf5f
18.isaacos <isaacabrahao940@gmail.com>
2024-09-26 20:43:01 -0300

    Add files via upload

---
+++ 12-tabuadaisaac.py
@@ -0,0 +1,15 @@
+
+input("Pressione Enter para ver as tabuadas de 1 a 10...")
+
+
+num = 1
+
+
+while num <= 10:
+    i = 1  
+    print(f"Tabuada do {num}:")  
+    while i <= 10:  
+        print(f"{num} x {i} = {num * i}")  
+        i += 1  
+    print()  
+    num += 1  
