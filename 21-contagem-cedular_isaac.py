a2754f91ea60deb9ac8b740869b96d6e8f0b3052
18.isaacos <isaacabrahao940@gmail.com>
2024-10-16 17:35:40 -0300

    Add files via upload

---
+++ 21-contagem-cedular_isaac.py
@@ -0,0 +1,15 @@
+# cedulas disponiveis
+cedulas = {50, 20, 10, 5, 1}
+valor = int(input('digite o valor total a ser pago (ou zero para sair): '))
+
+valor_original = valor
+print(f'\npara pagar o valor de R$ {valor_original}, serão necessarias: ')
+
+for cedula in cedulas:
+    if valor >= cedula:
+        quantidade = valor // cedula
+        print(f'R$ {cedula}: {quantidade} cédula(s)')
+        valor %= cedula
+    else:
+        print(f'R$ {cedula}: 0 cédula(s)')
+        print('\npagamento concluido,\n')
